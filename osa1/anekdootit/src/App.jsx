de34e35511af885de2b40c5f7e16ea852aa6fba3
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-04-06 21:38:23 +0300

    Updated anecddote vote array

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-  const [votes,setVotes] = useState([0,0,0,0,0,0,0,0])
+  const [votes,setVotes] = useState(new Array(anecdotes.length).fill(0))
   const [selected, setSelected] = useState(0)
 
   const randomElement = () => Math.floor(Math.random() * anecdotes.length)
