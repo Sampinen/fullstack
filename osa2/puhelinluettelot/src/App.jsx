bb5b7b2045beed0925cb6a011c0168e346628662
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-04-10 15:55:45 +0300

    Tehtävä 2.8

--- osa2/puhelinluettelot/src/App.jsx
+++ osa2/puhelinluettelot/src/App.jsx
@@ -3,27 +3,31 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
+      number: '044 6789523',
       id: 1
      }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const AddPerson = (event) => {
     event.preventDefault()
     console.log(event.target)
     const duplicate = persons.find((person)=>person.name === newName)
     if (duplicate) {
-      console.log("duplicate found")
+      console.log("duplicate name found")
       alert(newName + " is already added to phonebook")
     }
     else {
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length +1
     }
   
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   }
@@ -32,6 +36,11 @@ const App = () => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) =>{
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
 
 
   return (
@@ -43,13 +52,17 @@ const App = () => {
           onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value = {newNumber}
+          onChange = {handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-        <p key={person.id}> {person.name}</p>
+        <p key={person.id}> {person.name} {person.number}</p>
 
         ) }
       </ul>
