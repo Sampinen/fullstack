48147e37b64bf4526a1cefd5687b63509f7c4450
Sampinen <siiri.kalliomaki@helsinki.fi>
2026-09-14 15:26:28 +0300

    Vaihdettu backend portti

--- osa3/puhelinluettelot_backend/index.js
+++ osa3/puhelinluettelot_backend/index.js
@@ -111,7 +111,7 @@ app.get('/info', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 10000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
