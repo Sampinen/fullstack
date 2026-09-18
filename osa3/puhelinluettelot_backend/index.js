918dbe11f4864539ebd9bf1052c5c8683499e7ed
Sampinen <149503786+Sampinen@users.noreply.github.com>
2026-09-18 15:08:06 +0300

    Tehtävä 3.15

--- osa3/puhelinluettelot_backend/index.js
+++ osa3/puhelinluettelot_backend/index.js
@@ -58,23 +58,32 @@ app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
       response.json(persons)
 })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-   if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }    
+})
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response,next) => {
+  Person.findByIdAndDelete(request.params.id)    
+  .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
 
 
 app.post('/api/persons', (request, response) => {
@@ -120,6 +129,31 @@ app.get('/info', (request, response) => {
         <p>Date:${date.getDate()}.${date.getMonth()}.${date.getFullYear()} (DMY) Time:${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} GMT+0200(Eastern European standard time) </p>`
     )
 })
+
+
+//Error handling
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+
+app.use(errorHandler)
+
 
 
 const PORT = process.env.PORT || 10000
