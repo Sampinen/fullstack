f6c0bd6610afb669a0bf84f8dd4eab70445eb2c8
Sampinen <149503786+Sampinen@users.noreply.github.com>
2026-09-18 19:49:03 +0300

    uibuild

--- osa3/puhelinluettelot/src/App.jsx
+++ osa3/puhelinluettelot/src/App.jsx
@@ -178,7 +178,7 @@ const App = () => {
       setAlertType(null)
     }, 5000)
     }).catch(error => {
-      setAlertMessage(error.response.data.json())
+      setAlertMessage(error.response.json().data)
       setAlertType("error")
       setTimeout(() => {
         setAlertMessage(null)
